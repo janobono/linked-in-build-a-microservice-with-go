43a884d10f407d45b80cbb1fa938789a872c4028
janobono <janobono@gmail.com>
2024-04-08 09:30:40 +0200

    Create operations
    Signed-off-by: janobono <janobono@gmail.com>

--- internal/database/database_customer.go
+++ internal/database/database_customer.go
@@ -2,6 +2,10 @@ package database
 
 import (
 	"context"
+	"errors"
+	"github.com/google/uuid"
+	"github.com/janobono/linked-in-build-a-microservice-with-go/internal/dberrors"
+	"gorm.io/gorm"
 
 	"github.com/janobono/linked-in-build-a-microservice-with-go/internal/models"
 )
@@ -12,4 +16,17 @@ func (c client) GetAllCustomers(ctx context.Context, emailAddress string) ([]mod
 		Where(models.Customer{Email: emailAddress}).
 		Find(&customers)
 	return customers, result.Error
+}
+
+func (c client) AddCustomer(ctx context.Context, customer *models.Customer) (*models.Customer, error) {
+	customer.CustomerID = uuid.NewString()
+	result := c.DB.WithContext(ctx).
+		Create(&customer)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return nil, &dberrors.ConflictError{}
+		}
+		return nil, result.Error
+	}
+	return customer, nil
 }
--- internal/server/customer.go
+++ internal/server/customer.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/janobono/linked-in-build-a-microservice-with-go/internal/dberrors"
+	"github.com/janobono/linked-in-build-a-microservice-with-go/internal/models"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -14,4 +16,21 @@ func (s *EchoServer) GetAllCustomers(ctx echo.Context) error {
 		return ctx.JSON(http.StatusInternalServerError, err)
 	}
 	return ctx.JSON(http.StatusOK, customers)
+}
+
+func (s *EchoServer) AddCustomer(ctx echo.Context) error {
+	customer := new(models.Customer)
+	if err := ctx.Bind(customer); err != nil {
+		return ctx.JSON(http.StatusUnsupportedMediaType, err)
+	}
+	customer, err := s.DB.AddCustomer(ctx.Request().Context(), customer)
+	if err != nil {
+		switch err.(type) {
+		case *dberrors.ConflictError:
+			return ctx.JSON(http.StatusConflict, err)
+		default:
+			return ctx.JSON(http.StatusInternalServerError, err)
+		}
+	}
+	return ctx.JSON(http.StatusCreated, customer)
 }
